021ae6de4d55372fb998dd71578994a39d314268
mwitamarwa <106369843+mwitamarwa@users.noreply.github.com>
2022-07-17 00:53:48 +0300

    Add files via upload

---
+++ quadratic formula 1.c
@@ -0,0 +1,22 @@
+/*Quadratic formula
+ created by Mwita David
+ on 15 july 2022
+ C89 compiler*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float a,b,c;
+    float root1,root2;
+    float root_part,denom;
+    printf("enter values of a,b and c!\n");
+    scanf("%f%f%f",&a,&b,&c);
+    root_part = sqrt(b*b-4*a*c);
+    denom   =2*a;
+    root1 =(-b+root_part)/denom;
+    root2 =(-b-root_part)/denom;
+    printf("root1 = %\nroot2 =%f",root1,root2);
+
+    return 0;
+}
